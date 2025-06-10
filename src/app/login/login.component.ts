31b64347fea12c42e0b5069ec9cf695f9a19109b
SungJinwoo <vojincocovic@gmail.com>
2025-06-10 03:44:14 +0200

    login button add

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule }      from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule }     from '@angular/material/input';
+import { MatButtonModule }    from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
