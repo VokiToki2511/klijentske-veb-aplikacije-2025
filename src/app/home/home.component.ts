7cf5a151486dc3334dd0ba464e1d36f7710505cf
SungJinwoo <vojincocovic@gmail.com>
2025-06-10 22:46:16 +0200

    Implemented if and for each loops

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { AxiosError } from 'axios';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [JsonPipe,NgIf,NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  movies: any [] | null = null;
+  error: string | null = null
+
+  constructor(){
+    MovieService.getMovies()
+      .then(rsp=>this.movies = rsp.data)
+      .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`)
+
+  }
 
 }
