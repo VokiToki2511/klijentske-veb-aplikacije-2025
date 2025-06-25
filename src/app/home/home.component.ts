ece0f902d1770a46c1b48113ffce31a90c99e578
SungJinwoo <vojincocovic@gmail.com>
2025-06-25 03:20:33 +0200

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
-import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import {  NgFor, NgIf } from '@angular/common';
 import { AxiosError } from 'axios';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [JsonPipe,NgIf,NgFor,MatCardModule, MatButtonModule],
+  imports: [NgIf,NgFor,MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -16,7 +16,7 @@ export class HomeComponent {
   error: string | null = null
 
   constructor(){
-    MovieService.getMovies()
+    MovieService.getMovies(0, 3)
       .then(rsp=>this.movies = rsp.data)
       .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`)
 
